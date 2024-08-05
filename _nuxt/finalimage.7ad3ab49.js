6404b69dc24298bcbf6e8208f2a032ce2a5fc9f9
Juan Miguel Lugo Parra <juanmi679@gmail.com>
2024-08-05 19:13:23 -0400

    first commit

--- _nuxt/finalimage.7ad3ab49.js
+++
@@ -1,1 +0,0 @@
-import"./entry.df603ac4.js";const r=""+new URL("finalimage.f16ac972.mp4",import.meta.url).href;export{r as _};
---
+++ _nuxt/finalimage.dad9e5c9.js
@@ -0,0 +1,1 @@
+import"./entry.f7abbd97.js";const r=""+new URL("finalimage.f16ac972.mp4",import.meta.url).href;export{r as _};
