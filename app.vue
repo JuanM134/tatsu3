d98fc72dc3647f7015ac5993a3fcc4bc3f432191
JuanM134 <69129238+JuanM134@users.noreply.github.com>
2025-09-13 17:00:00 -0400

    Delete app.vue

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!-- z-index: 1 allows navbar to stay on top when using it with other pages
-  like the home page which have a video in the background. Home page background video has to set z-index: 0-->
-  <!-- <navbar style="z-index: 1;" /> -->
-
-  <div id="main-container">
-    <NuxtPage />
-  </div>
-</template>
