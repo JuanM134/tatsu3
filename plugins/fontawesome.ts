47f40be0ec06952f91f50f20cbe33800bc0ae2ac
JuanM134 <69129238+JuanM134@users.noreply.github.com>
2025-10-26 17:04:28 -0400

    Refactor FontAwesome icon imports and registration

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,35 +1,27 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { faMinus } from '@fortawesome/free-solid-svg-icons'
-import { faEquals } from '@fortawesome/free-solid-svg-icons'
-import { faXTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faDiscord } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
+// Importa los estilos globales de FontAwesome (¡importante!)
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
-// This is important, we are going to let Nuxt worry about the CSS
+// Importa los íconos que usarás
+import { 
+  faArrowDown, faArrowUp, faArrowLeft, faPlus, faMinus, faEquals, faHouse 
+} from '@fortawesome/free-solid-svg-icons'
+import { 
+  faXTwitter, faDiscord, faInstagram 
+} from '@fortawesome/free-brands-svg-icons'
+
+// Evita que FontAwesome inserte CSS automáticamente (Nuxt lo maneja)
 config.autoAddCss = false
 
-// You can add your icons directly in this plugin. See other examples for how you
-// can add other styles or just individual icons.
-library.add(faArrowDown)
-library.add(faArrowUp)
-library.add(faArrowLeft)
-library.add(faPlus)
-library.add(faMinus)
-library.add(faEquals)
-library.add(faXTwitter)
-library.add(faDiscord)
-library.add(faInstagram)
-library.add(faHouse)
+// Añade los íconos a la librería
+library.add(
+  faArrowDown, faArrowUp, faArrowLeft, faPlus, faMinus, faEquals,
+  faXTwitter as any, faDiscord as any, faInstagram as any, faHouse as any
+)
 
-
-
+// Registra el componente globalmente
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
+  nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
 })
