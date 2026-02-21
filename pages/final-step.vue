e076046bcf81068e32f52913ddda23492980ff98
JuanM134 <69129238+JuanM134@users.noreply.github.com>
2026-02-21 19:28:04 -0400

    Actualizar final-step.vue

--- pages/final-step.vue
+++ pages/final-step.vue
@@ -14,7 +14,7 @@
       </div>
 
       <a 
-        href="https://discord.gg/t9CzNNg9" 
+        href=" " 
         target="_blank" 
         class="enter-btn"
         :class="{ 'enter-btn-active-bg': isActive }"
